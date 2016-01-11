0b4b2ff0421b7b5247fd5e71e205c40bb9a14fdb
yuta-ys0603 <yu06ta03@gmail.com>
2016-01-11 21:38:28 +0900

    modify user destroy function not to destroy other user's info

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -63,8 +63,8 @@ class ImagesController < ApplicationController
   # DELETE /images/1.json
   def destroy
     if @current_user.id == @image.user.id
-    @image.destroy
-    respond_to do |format|
+     @image.destroy
+     respond_to do |format|
       format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
       format.json { head :no_content }
     end
